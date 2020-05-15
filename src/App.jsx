78d1ca3bfd72e47bbef16570894ab48d1f0c5587
Dipz <dipz@dipzs-air.home>
2020-05-15 20:09:22 +0100

    addedBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "./App.css";
 import Player from "./components/Player.jsx";
 
@@ -15,9 +15,13 @@ function App() {
           <Player player="dipz" score="5" />
         </Col>
 
-        <Col style={{ border: "dashed blue", padding: "1em" }} sm="6">
+        <Col
+          className="d-flex flex-column align-items-center"
+          style={{ border: "dashed blue", padding: "1em" }}
+          sm="6"
+        >
           <span>"PLAYERS-NAME" It's your turn</span>
-          <p>tic tac toe game</p>
+          <Board />
         </Col>
 
         <Col style={{ border: "dashed yellow", padding: "1em" }} sm="3">
@@ -29,3 +33,53 @@ function App() {
 }
 
 export default App;
+
+function Square(props) {
+  return (
+    <Button className="square" onClick={props.onClick}>
+      {props.value}
+    </Button>
+  );
+}
+
+function Board() {
+  const [boardSquares, setBoardSquares] = useState(Array(9).fill(null));
+
+  const [xIsNext, setXisNext] = useState(true);
+
+  const handleClick = (index) => {
+    const currentSquares = [...boardSquares];
+    //check if square has already been clicked
+    if (currentSquares[index] !== null) return;
+
+    currentSquares[index] = xIsNext ? "X" : "O";
+    setXisNext(!xIsNext);
+    setBoardSquares(currentSquares);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <Square value={boardSquares[index]} onClick={() => handleClick(index)} />
+    );
+  };
+
+  return (
+    <div>
+      <div>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
