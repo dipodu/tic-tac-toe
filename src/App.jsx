ac37402400d68acf6bfd550360819e7fe9ca2d03
Dipz <dipz@dipzs-air.home>
2020-05-15 21:55:35 +0100

    refactorBoared

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "./App.css";
 import Player from "./components/Player.jsx";
+import Board from "./components/Board.jsx";
 
 function App() {
-  return (
-    <Container id="main">
-      <Row className="justify-content-center">
-        <h1>TIC TAC TOE GAME</h1>
-      </Row>
-
-      <Row style={{ border: "dashed red", height: "80%", padding: "2em" }}>
-        <Col style={{ border: "dashed yellow", padding: "1em" }} sm="3">
-          <Player player="dipz" score="5" />
-        </Col>
-
-        <Col
-          className="d-flex flex-column align-items-center"
-          style={{ border: "dashed blue", padding: "1em" }}
-          sm="6"
-        >
-          <span>"PLAYERS-NAME" It's your turn</span>
-          <Board />
-        </Col>
-
-        <Col style={{ border: "dashed yellow", padding: "1em" }} sm="3">
-          <Player player="ola" score="9" />
-        </Col>
-      </Row>
-    </Container>
-  );
-}
-
-export default App;
-
-function Square(props) {
-  return (
-    <Button className="square" onClick={props.onClick}>
-      {props.value}
-    </Button>
-  );
-}
-
-function Board() {
   const [boardSquares, setBoardSquares] = useState(Array(9).fill(null));
-
   const [xIsNext, setXisNext] = useState(true);
 
   const handleClick = (index) => {
@@ -57,29 +18,41 @@ function Board() {
     setBoardSquares(currentSquares);
   };
 
-  const renderSquare = (index) => {
-    return (
-      <Square value={boardSquares[index]} onClick={() => handleClick(index)} />
-    );
-  };
-
   return (
-    <div>
-      <div>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
+    <Container id="main">
+      <Row className="justify-content-center">
+        <h1>TIC TAC TOE GAME</h1>
+      </Row>
+
+      <Row
+        style={{
+          border: "dashed red",
+          height: "80%",
+          padding: "2em",
+        }}
+      >
+        <Col style={{ border: "dashed yellow", padding: "1em" }} sm="3">
+          <Player player="dipz" score="5" />
+        </Col>
+
+        <Col
+          className="d-flex flex-column align-items-center"
+          style={{ border: "dashed blue", padding: "1em" }}
+          sm="6"
+        >
+          <Board
+            stateOfBoard={boardSquares}
+            xTurn={xIsNext}
+            onClickSquare={handleClick}
+          />
+        </Col>
+
+        <Col style={{ border: "dashed yellow", padding: "1em" }} sm="3">
+          <Player player="ola" score="9" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
+
+export default App;
---
+++ src/components/Board.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export default function Board(props) {
+  const { stateOfBoard, xTurn, onClickSquare } = props;
+
+  const renderSquare = (index) => {
+    return (
+      <Square
+        value={stateOfBoard[index]}
+        onClick={() => onClickSquare(index)}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <span>"PLAYERS-NAME" It's your turn</span>
+
+      <div>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+function Square(props) {
+  return (
+    <Button className="square" onClick={props.onClick}>
+      {props.value}
+    </Button>
+  );
+}
